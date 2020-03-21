3f064cde872fef39815d2adfda1c70924fd1fdb6
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2020-03-21 11:43:55 +0100

    Add logos in About page

--- client/src/components/core/About/script.js
+++ client/src/components/core/About/script.js
@@ -1,3 +1,13 @@
+import arizona from 'parflow-web/src/assets/logo-arizona.png';
+import igwmc from 'parflow-web/src/assets/logo-igwmc.png';
+import kitware from 'parflow-web/src/assets/logo-kitware.svg';
+import mines from 'parflow-web/src/assets/logo-mines.jpg';
+
 export default {
   name: 'About',
+  data() {
+    return {
+      logos: [kitware, arizona, igwmc, mines],
+    };
+  },
 };
