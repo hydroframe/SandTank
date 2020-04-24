11f9c7a860a937955579626231d9774913f3e544
Patrick Avery <patrick.avery@kitware.com>
2020-04-24 05:35:43 -0400

    Rename github token to GITHUB_TOKEN
    semantic-release specificaly looks for "GITHUB_TOKEN" or
    "GH_TOKEN" in the environment. We need to use one of these.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- scripts/github-actions/publish_docs.sh
+++ scripts/github-actions/publish_docs.sh
@@ -3,6 +3,6 @@ set -ev
 
 git config --global user.name "hydroframe-bot"
 git config --global user.email "hydroframe.bot@gmail.com"
-export GIT_PUBLISH_URL=https://${HYDROFRAME_BOT_TOKEN}@github.com/hydroframe/SandTank.git
+export GIT_PUBLISH_URL=https://${GITHUB_TOKEN}@github.com/hydroframe/SandTank.git
 npm run semantic-release
 npm run doc:publish
