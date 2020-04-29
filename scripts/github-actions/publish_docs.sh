65d5e5b8a841363f346298ebc473072be62a9ee2
Patrick Avery <patrick.avery@kitware.com>
2020-04-29 17:31:09 -0400

    fix(semantic-release): set git environment variables
    This will modify the name/email of the committer from semantic-release-bot to hydroframe-bot.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- scripts/github-actions/publish_docs.sh
+++ scripts/github-actions/publish_docs.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 set -ev
 
-git config --global user.name "hydroframe-bot"
-git config --global user.email "hydroframe.bot@gmail.com"
+BOT_NAME="hydroframe-bot"
+BOT_EMAIL="hydroframe.bot@gmail.com"
+
+git config --global user.name "$BOT_NAME"
+git config --global user.email "$BOT_EMAIL"
 export GIT_PUBLISH_URL=https://${GITHUB_TOKEN}@github.com/hydroframe/SandTank.git
+
+# Environment variables for semantic-release
+export GIT_AUTHOR_NAME=$BOT_NAME
+export GIT_AUTHOR_EMAIL=$BOT_EMAIL
+export GIT_COMMITTER_NAME=$BOT_NAME
+export GIT_COMMITTER_EMAIL=$BOT_EMAIL
+
 npm run semantic-release
 npm run doc:publish
