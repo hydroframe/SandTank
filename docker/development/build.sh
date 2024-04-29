6a09d8d008226793c738eb2839c251fd8d2c880b
wh3248 <wh3248@princeton.edu>
2024-04-29 10:03:35 -0400

    build with podman

--- docker/development/build.sh
+++ docker/development/build.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
-# This will be ran with the root directory as the docker context
+# This builds the sandtank-dev docker image from the git repo pull in the current directory
+# This is the first base image of the sandtank application. This is used by the runtime docker image build.
+# This will be run with the root directory as the docker context
+
+# Usage to build using docker (on a laptop):
+#   bash build.sh
+# Usage to build using podman (on Princeton linux servers that use podman)
+#   bash build.sh podman
 
 SCRIPT_DIR=`dirname "$0"`
 ROOT_DIR=$SCRIPT_DIR/../..
-docker build -t sandtank-dev -f $SCRIPT_DIR/Dockerfile $ROOT_DIR
+DOCKER=$1
+if [[ $DOCKER == "" ]]; then DOCKER=docker ; fi
+
+# Remove old docker image and any dangling (non tagged images)
+# This prevents CI/CD builds from accumulating old copies of docker images
+${DOCKER} image rm sandtank-dev:latest
+${DOCKER} rmi $(${DOCKER} images -f "dangling=true" -q)
+
+# Build docker image of sandtank dev
+$DOCKER build -t sandtank-dev -f $SCRIPT_DIR/Dockerfile $ROOT_DIR
--- docker/web/build.sh
+++ docker/web/build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=`dirname "$0"`
-docker build -t hydroframe/sandtank:latest $SCRIPT_DIR
+DOCKER=$1
+TAG=$2
+if [[ $DOCKER == "" ]]; then DOCKER=docker ; fi
+if [[ $TAG == "" ]]; then TAG=test ; fi
+
+# Remove old docker image and any dangling (non tagged images)
+# This prevents CI/CD builds from accumulating old copies of docker images
+${DOCKER} image rm sandtank:${TAG}
+${DOCKER} rmi $(${DOCKER} images -f "dangling=true" -q)
+
+$DOCKER build -t localhost/sandtank:${TAG} $SCRIPT_DIR
