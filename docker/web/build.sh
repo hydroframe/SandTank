f6de58623a210e1f21bbfc91db699910c5a62958
wh3248 <wh3248@princeton.edu>
2024-05-17 12:08:28 -0400

    update run.tcl to add velfile True to slimin.txt for new ecoSlim version

--- docker/web/build.sh
+++ docker/web/build.sh
@@ -12,3 +12,5 @@ ${DOCKER} image rm sandtank:${TAG}
 ${DOCKER} rmi $(${DOCKER} images -f "dangling=true" -q)
 
 $DOCKER build -t localhost/sandtank:${TAG} $SCRIPT_DIR
+$DOCKER tag sandtank:${TAG} docker.io/hydroframe/sandtank:${TAG}
+$DOCKER push docker.io/hydroframe/sandtank:${TAG}
